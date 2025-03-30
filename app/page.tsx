fc4b868cc25764d489b161824d421cb4e872c257
CrusaderGoT <enememeka44@gmail.com>
2025-03-30 16:31:02 +0100

    refactor: added tabler icon. removed: colony circle. removed: components.json/shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,86 +1,9 @@
-import index from "@/styles/index.module.css";
-import Image from "next/image";
+import { Box } from "@mantine/core";
 
-export default function Home() {
+export default async function HomePage() {
     return (
-        <div
-            className="
-            bg-gradient-rzadial dark:from-slate-400 dark:to-slate-900 to-slate-400 from-slate-900
-            font-mono text-sm flex items-center min-h-screen
-            justify-center relative"
-        >
-            <ColonyCircle />
-        </div>
-    );
-}
-
-function ColonyCircle() {
-    return (
-        <div
-            className={`
-                ${index.colonyCircle} p-5 overflow-hidden w-[380px] h-[380px] sm:w-[600px] sm:h-[600px] md:w-[700px] md:h-[700px] rounded-full cursor-default relative flex items-center justify-center container
-            `}
-        >
-            <ColonyBarrier />
-            <ColonyImg />
-            <CircleContent />
-        </div>
-    );
-}
-
-function ColonyBarrier() {
-    return (
-        <div
-            className={`${index.colonyBarrier} bg-[hsla(0,0%,0%,97%)]
-    absolute min-w-full min-h-full z-[4] rounded-full`}
-        ></div>
-    );
-}
-
-function ColonyImg() {
-    return (
-        <Image
-            src={`/images/images.jpeg-1.jpg`}
-            alt="city-skyline.jpg"
-            width={612}
-            height={408}
-            className="absolute w-full h-full rounded-full"
-            priority={false}
-        />
-    );
-}
-
-function CircleContent() {
-    return (
-        <div className="relative !text-white z-[6] rounded-full flex flex-col items-center justify-center w-full h-full">
-            <ColonyText />
-            <ColonyWarning />
-        </div>
-    );
-}
-
-function ColonyText() {
-    return (
-        <h2
-            className={`${index.colonyText} font-semibold
-      sm:font-bold lg:font-extrabold tracking-[10px]
-      md:tracking-[20px] lg:tracking-[30px]
-      text-lg sm:text-2xl md:text-3xl
-      absolute top-1/2 translate-y-[-50%]`}
-        >
-            COLONY
-        </h2>
-    );
-}
-
-function ColonyWarning() {
-    return (
-        <p
-            className={`${index.colonyWarn} absolute bottom-2 text-[10px]
-      leading-3 sm:text-xs text-wrap
-      text-center w-2/3 text-red-600 font-thin`}
-        >
-            a dangerous game is going on inside &#x1F6C8;
-        </p>
-    );
+        <Box>
+            Colony Circle
+        </Box>
+    )
 }
