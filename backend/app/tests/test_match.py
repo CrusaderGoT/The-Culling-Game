ab3cec9655d7e636086b1c02091eacb607da73a9
CrusaderGoT <enememeka44@gmail.com>
2025-04-16 14:14:35 +0100

    Refactored barrier APIs into dedicated routes. All test passing

--- backend/app/tests/test_match.py
+++ backend/app/tests/test_match.py
@@ -111,7 +111,7 @@ def test_domain_expansion(match_players: list[tuple[TestClient, dict]]):
     # activate for player 1
     player1 = match_players[0][1]
     res0 = match_players[0][0].post(
-        f"match/activate/domain/{player1['id']}", params={"match_id": 1}
+        f"/barrier/activate/domain/{player1['id']}", params={"match_id": 1}
     )
     assert res0.is_success is True, (
         f"Couldn't activate domain for player 1: {res0.json()}"
--- backend/app/utils/config.py
+++ backend/app/utils/config.py
@@ -1,40 +1,55 @@
-'''module for configurations of
+"""module for configurations of
 1. path operation decorators args; e.g tags.
 2. custom exceptions.
-3. Common logic'''
-from app.api.settings import app
-from app.models.user import UserInfo, User
-from fastapi import status, Request
-from fastapi.responses import JSONResponse
+3. Common logic"""
+
 from enum import Enum
+
+from fastapi import Request, status
+from fastapi.responses import JSONResponse
+
+from app.api.settings import app
+from app.models.user import User, UserInfo
 
 # configurations goes here
 
 # 1. EXCEPTIONS
 
+
 class UserException(Exception):
-    '''user custom exception; will convert the User to UserInfo'''
-    def __init__(self, user:User, code:int=status.HTTP_406_NOT_ACCEPTABLE,
-                 detail: str | dict={'msg': 'An Error Occured with this User'},
-                 headers: dict[str, str] | None = None) -> None:
+    """user custom exception; will convert the User to UserInfo"""
+
+    def __init__(
+        self,
+        user: User,
+        code: int = status.HTTP_406_NOT_ACCEPTABLE,
+        detail: str | dict = {"msg": "An Error Occured with this User"},
+        headers: dict[str, str] | None = None,
+    ) -> None:
         self.user = UserInfo.model_validate(user)
         self.err_msg = detail
         self.code = code
 
+
 @app.exception_handler(UserException)
-async def user_exception_handler(request:Request, exc:UserException):
+async def user_exception_handler(request: Request, exc: UserException):
     return JSONResponse(status_code=exc.code, content=exc.err_msg)
 
+
 class PlayerException(Exception):
-    '''custom player exception'''
+    """custom player exception"""
+
     pass
+
 
 # 2. TAGS for openapi, used to group path operators
 class Tag(str, Enum):
-    'tags for path operation decorators'
-    user = 'users'
-    player = 'players'
-    auth = 'auth'
-    match = 'matches'
-    admin = 'admin'
-    colony = 'colonies'
+    "tags for path operation decorators"
+
+    user = "users"
+    player = "players"
+    auth = "auth"
+    match = "matches"
+    admin = "admin"
+    colony = "colonies"
+    barrier = "barriers"
