54628d355efe56a9bc66f43806b7fc72b478cf6f
CrusaderGoT <enememeka44@gmail.com>
2025-05-22 03:22:43 +0100

    renamed file

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -23,7 +23,7 @@ import {
     charcoalColor,
     deepRedColor,
     goldColor,
-} from "@/lib/constants/THEMECOLORS";
+} from "@/lib/constants/themes";
 
 import TanstackProviders from "@/lib/query-client/providers";
 
