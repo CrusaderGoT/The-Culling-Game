2d311cf148335c1113978c526f12205d1acd5df9
CrusaderGoT <enememeka44@gmail.com>
2025-05-23 04:55:22 +0100

    minor enhancement

--- app/routers/matches.py
+++ app/routers/matches.py
@@ -235,7 +235,11 @@ async def vote(
 
                 else:  # runs after the loop
                     # make sure new votes will not exceed the 5 vote limit
-                    new_votes = new_votes[: (atp.vote_limit - len(prev_votes))]
+                    expected_limit = atp.vote_limit - len(prev_votes)
+                    real_limit = (
+                        expected_limit if expected_limit > 0 else 0
+                    )  # incase prev vote is > 5; i.e e_l results in a negative number
+                    new_votes = new_votes[:real_limit]
                     session.add_all(new_votes)
                     session.commit()  # this commit the increased player points also
                     [session.refresh(v) for v in new_votes]
--- app/tests/shit.py
+++ app/tests/shit.py
@@ -1,7 +1,10 @@
 c = [1, 2, 3, 5, 6]
-pv = 3
+pv = 4
 vl = 5
+e = vl - pv
 
-a = c[: (vl - pv)]
+b = e if e > 0 else 0
 
-print(a, vl - pv)
+a = c[:b]
+
+print(a, vl - pv, b)
