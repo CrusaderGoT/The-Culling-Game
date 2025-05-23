19952d03f9dca325a376e2ee44791c0b20d0f8f5
CrusaderGoT <enememeka44@gmail.com>
2025-05-23 04:47:20 +0100

    fix: made sure valid votes don't exceed vote limit.

--- app/tests/shit.py
+++ app/tests/shit.py
@@ -1,27 +1,7 @@
-from collections import Counter
-from uuid import uuid4
+c = [1, 2, 3, 5, 6]
+pv = 3
+vl = 5
 
-votes = [
-    {"player_id": 1, "point": 0.2},
-    {"player_id": 1, "point": 0.2},
-    {"player_id": 2, "point": 0.2},
-    {"player_id": 2, "point": 0.2},
-]
-cnt = Counter()  # initialize counter
-# aggregate players votes points
-for vote in votes:
-    cnt[vote["player_id"]] += vote["point"]
+a = c[: (vl - pv)]
 
-# get the player with most votes
-most_votes = cnt.most_common(1)[0]
-# get the player with least votes
-n = 1
-least_votes = cnt.most_common()[: -n - 1 : -1][0]
-
-if most_votes[1] == least_votes[1]:
-    print("draw")
-else:
-    print(most_votes[0], "won")
-
-
-print(most_votes, "+", least_votes, uuid4())
+print(a, vl - pv)
