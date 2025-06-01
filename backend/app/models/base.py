8f0bf9883a53c4da9f475838c8a9e74fa63ce28a
CrusaderGoT <enememeka44@gmail.com>
2025-06-01 06:25:59 +0100

    Revert "made base vote FK nullable agin. this is because it has to be that way for Fastapi. updated sdk. minor fixes"
    This reverts commit 230b88a826b7a4524bd60155bdd028002d8e8a6e.

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -356,20 +356,12 @@ class BaseBarrierTech(SQLModel):
 class BaseVote(SQLModel):
     """
     ### The base class for a vote
-    `player_id: int | None = Field(
-        default=None, foreign_key="player.id", ondelete="RESTRICT", index=True
-    )`
-    `ct_app_id: int | None = Field(
-        default=None, foreign_key="ctapp.id", ondelete="RESTRICT", index=True
-    )`
+    `player_id: int = Field(foreign_key="player.id", ondelete="RESTRICT")`
+    `ct_app_id: int = Field(foreign_key="ctapp.id", ondelete="RESTRICT")`
     """
 
-    player_id: int | None = Field(
-        default=None, foreign_key="player.id", ondelete="RESTRICT", index=True
-    )
-    ct_app_id: int | None = Field(
-        default=None, foreign_key="ctapp.id", ondelete="RESTRICT", index=True
-    )
+    player_id: int = Field(foreign_key="player.id", ondelete="RESTRICT", index=True)
+    ct_app_id: int = Field(foreign_key="ctapp.id", ondelete="RESTRICT", index=True)
 
 
 class ActionTimePoint(SQLModel):
--- frontend/api/client/zod.gen.ts
+++ frontend/api/client/zod.gen.ts
@@ -319,14 +319,8 @@ export const zBaseMatchInfo = z.object({
 });
 
 export const zBaseVoteInfo = z.object({
-    player_id: z.union([
-        z.number().int(),
-        z.null()
-    ]).optional(),
-    ct_app_id: z.union([
-        z.number().int(),
-        z.null()
-    ]).optional(),
+    player_id: z.number().int(),
+    ct_app_id: z.number().int(),
     id: z.number().int(),
     user_id: z.number().int(),
     point: z.number(),
