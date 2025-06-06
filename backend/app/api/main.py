b6d529daff5b75e6f2a72f8a0b0a9f13ecaf4510
CrusaderGoT <enememeka44@gmail.com>
2025-06-06 04:41:22 +0100

    made sleek homepage for the backend

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -218,7 +218,7 @@ async def custom_swagger_ui_html():
         oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
         swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
         swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
-        swagger_favicon_url="/static/images/Kogane.png",
+        swagger_favicon_url="static/favicon.ico",
     )
 
 
